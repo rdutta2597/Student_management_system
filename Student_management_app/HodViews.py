c2debd68891b6e4ac00df09831a424b2d2c7bb15
rdutta2597 <36539802+rdutta2597@users.noreply.github.com>
2021-08-24 21:14:45 +0530

    Initial Commit

--- Student_management_app/HodViews.py
+++ Student_management_app/HodViews.py
@@ -1,5 +1,100 @@
+import datetime
+
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+
+from Student_management_app.models import CustomUser, Courses
 
 
 def admin_home(request):
-    return render(request,'hod_template/home_content.html')
+    return render(request, 'hod_template/home_content.html')
+
+
+def add_staff(request):
+    return render(request, "hod_template/add_staff_template.html")
+
+
+def add_staff_save(request):
+    if request.method != "POST":
+        return HttpResponse("Method not allowed")
+    else:
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        address = request.POST.get("address")
+        try:
+            user = CustomUser.objects.create_user(username=username, password=password, email=email,
+                                                  last_name=last_name,
+                                                  first_name=first_name, user_type=2)
+            user.staffs.address = address
+            user.save()
+            messages.success(request, "Successfully added staff")
+            return HttpResponseRedirect("/add_staff")
+
+        except:
+            messages.error(request, "Failed to add staff")
+            return HttpResponseRedirect("/add_staff")
+
+
+def add_course(request):
+    return render(request, "hod_template/add_course_template.html")
+
+
+def add_course_save(request):
+    if request.method != "POST":
+        return HttpResponseRedirect("Method Not Allowed")
+    else:
+        course = request.POST.get("course")
+        try:
+            course_model = Courses(course_name=course)
+            course_model.save()
+            messages.success(request, "Successfully added course")
+            return HttpResponseRedirect("/add_course")
+        except:
+            messages.error(request, "Failed to add course")
+            return HttpResponseRedirect("/add_course")
+
+
+def add_student(request):
+    courses=Courses.objects.all()
+    return render(request, "hod_template/add_student_template.html",{"courses":courses})
+
+
+def add_student_save(request):
+    if request.method != "POST":
+        return HttpResponse("Method not allowed")
+    else:
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        address = request.POST.get("address")
+        session_start = request.POST.get("session_start")
+        session_end = request.POST.get("session_end")
+        course_id = request.POST.get("course")
+        sex = request.POST.get("sex")
+
+        try:
+            user = CustomUser.objects.create_user(username=username, password=password, email=email,
+                                                  last_name=last_name,
+                                                  first_name=first_name, user_type=3)
+            user.students.address = address
+            course_obj = Courses.objects.get(id=course_id)
+            user.students.course_id=course_obj
+            start_date = datetime.datetime.striptime(session_start,'%d-%m-%y').strftime('%Y-%m-%d')
+            end_date = datetime.datetime.striptime(session_end, '%d-%m-%y').strftime('%Y-%m-%d')
+            user.students.session_start_year=start_date
+            user.students.session_end_year = end_date
+            user.students.gender=sex
+            user.students.profile_pic=""
+            user.save()
+            messages.success(request, "Successfully added student")
+            return HttpResponseRedirect("/add_student")
+
+        except:
+            messages.error(request, "Failed to add student")
+            return HttpResponseRedirect("/add_staff")
--- Student_management_app/models.py
+++ Student_management_app/models.py
@@ -8,13 +8,14 @@ from django.dispatch import receiver
 class CustomUser(AbstractUser):
     user_type_data = ((1, "HOD"), (2, "Staff"), (3, "Student"))
     user_type = models.CharField(default=1, choices=user_type_data, max_length=10)
+
+
 class AdminHOD(models.Model):
     id = models.AutoField(primary_key=True)
     admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
-
 
 
 class Staffs(models.Model):
