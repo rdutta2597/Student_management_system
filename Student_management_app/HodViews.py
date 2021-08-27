92d09ea4a52dd37fa94c77bef93bcad5225fc94b
rdutta2597 <36539802+rdutta2597@users.noreply.github.com>
2021-08-27 23:59:17 +0530

    Initial Commit

--- Student_management_app/HodViews.py
+++ Student_management_app/HodViews.py
@@ -4,11 +4,11 @@ from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
-from Student_management_app.models import CustomUser, Courses
+from Student_management_app.models import CustomUser, Courses, Subjects, Staffs, Students
 
 
 def admin_home(request):
-    return render(request, 'hod_template/home_content.html')
+    return render(request, "hod_template/home_content.html")
 
 
 def add_staff(request):
@@ -17,7 +17,7 @@ def add_staff(request):
 
 def add_staff_save(request):
     if request.method != "POST":
-        return HttpResponse("Method not allowed")
+        return HttpResponse("Method Not Allowed")
     else:
         first_name = request.POST.get("first_name")
         last_name = request.POST.get("last_name")
@@ -27,15 +27,13 @@ def add_staff_save(request):
         address = request.POST.get("address")
         try:
             user = CustomUser.objects.create_user(username=username, password=password, email=email,
-                                                  last_name=last_name,
-                                                  first_name=first_name, user_type=2)
+                                                  last_name=last_name, first_name=first_name, user_type=2)
             user.staffs.address = address
             user.save()
-            messages.success(request, "Successfully added staff")
+            messages.success(request, "Successfully Added Staff")
             return HttpResponseRedirect("/add_staff")
-
         except:
-            messages.error(request, "Failed to add staff")
+            messages.error(request, "Failed to Add Staff")
             return HttpResponseRedirect("/add_staff")
 
 
@@ -45,16 +43,16 @@ def add_course(request):
 
 def add_course_save(request):
     if request.method != "POST":
-        return HttpResponseRedirect("Method Not Allowed")
+        return HttpResponse("Method Not Allowed")
     else:
         course = request.POST.get("course")
         try:
             course_model = Courses(course_name=course)
             course_model.save()
-            messages.success(request, "Successfully added course")
+            messages.success(request, "Successfully Added Course")
             return HttpResponseRedirect("/add_course")
         except:
-            messages.error(request, "Failed to add course")
+            messages.error(request, "Failed To Add Course")
             return HttpResponseRedirect("/add_course")
 
 
@@ -97,17 +95,47 @@ def add_student_save(request):
             messages.error(request, "Failed to add student")
             return HttpResponseRedirect("/add_student")
 
+
 def add_subject(request):
     courses = Courses.objects.all()
-    staffs=CustomUser.objects.filter(user_type=2)
-    return render(request,"hod_template/add_subject_template.html",{"staffs":staffs,"courses":courses})
+    staffs = CustomUser.objects.filter(user_type=2)
+    return render(request, "hod_template/add_subject_template.html", {"staffs": staffs, "courses": courses})
+
 
 def add_subject_save(request):
-    if request.method!="POST"
-        return HttpResponse("<h2>Method Not allowed</h2>")
+    if request.method != "POST":
+        return HttpResponse("<h2>Method Not Allowed</h2>")
     else:
-        subject_name=request.POST.get("subject_name")
-        course_id=request.POST.get("course")
+        subject_name = request.POST.get("subject_name")
+        course_id = request.POST.get("course")
+        course = Courses.objects.get(id=course_id)
         staff_id = request.POST.get("staff")
-        staff = CustomUser.objects.get(id=course_id)
+        staff = CustomUser.objects.get(id=staff_id)
 
+        try:
+            subject = Subjects(subject_name=subject_name, course_id=course, staff_id=staff)
+            subject.save()
+            messages.success(request, "Successfully Added Subject")
+            return HttpResponseRedirect("/add_subject")
+        except:
+            messages.error(request, "Failed to Add Subject")
+            return HttpResponseRedirect("/add_subject")
+
+
+def manage_staff(request):
+    staffs = Staffs.objects.all
+    return render(request, "hod_template/manage_staff_template.html", {"staffs": staffs})
+
+
+def manage_student(request):
+    students = Students.objects.all()
+    return render(request, "hod_template/manage_student_template.html", {"students": students})
+
+
+def manage_course(request):
+    courses = Courses.objects.all()
+    return render(request, "hod_template/manage_course_template.html", {"courses": courses})
+
+def manage_subject(request):
+    subjects=Subjects.objects.all()
+    return render(request,"hod_template/manage_subject_template.html",{"subjects":subjects})
